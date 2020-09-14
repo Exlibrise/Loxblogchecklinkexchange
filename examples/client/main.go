20ef06f5b08cccedafa2d7425e5df238464437f7
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-09-14 15:55:06 +0000

    final report

--- examples/client/main.go
+++ examples/client/main.go
@@ -12,3 +12,5 @@ func main() {
 	// Set up client
 	lndOptions := ln.LNDoptions{ // Default address: "localhost:10009", CertFile: "tls.cert"
 		MacaroonFile: "admin.macaroon", // admin.macaroon is required for making payments
+	}
+	lnClient, err := ln.NewLND

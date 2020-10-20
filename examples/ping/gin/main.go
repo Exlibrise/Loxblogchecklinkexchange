77799b1756bbd0ef5e5e0140895a60a971507611
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-10-20 17:07:08 +0000

    Preparing release 1.2

--- examples/ping/gin/main.go
+++ examples/ping/gin/main.go
@@ -14,4 +14,5 @@ func main() {
 
 	// Configure middleware
 	invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
-	lndOptions := ln.DefaultLNDoptions           // Address: "localhost:10009", CertFile: "tls.cert", MacaroonFile: "invoice.m
+	lndOptions := ln.DefaultLNDoptions           // Address: "localhost:10009", CertFile: "tls.cert", MacaroonFile: "invoice.macaroon"
+	storageClient :=

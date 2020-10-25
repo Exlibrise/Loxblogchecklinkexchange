b284627041c598649e443d40c82538fd15ffc2c6
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-10-25 07:42:54 +0000

    PCI/MSI: Add device flag indicating that 64-bit MSIs don't work

--- examples/ping/gin-charge/main.go
+++ examples/ping/gin-charge/main.go
@@ -1,2 +1,27 @@
 
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/philippgille/ln-paywall/ln"
+	"github.com/philippgille/ln-paywall/storage"
+	"github.com/philippgille/ln-paywall/wall"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Configure middleware
+	invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
+	chargeOptions := ln.ChargeOptions{           // Address: "http://localhost:9112"
+		APItoken: "secret",
+	}
+	storageClient := storage.NewGoMap() // Local in-memory cache
+	lnClient, err := ln.NewChargeClient(chargeOptions)
+	if err != nil {
+		panic(err)
+	}
+	// Use middleware
+	r.Use(wall.NewGinMiddleware(invoiceOptions, lnClient, storageClient))

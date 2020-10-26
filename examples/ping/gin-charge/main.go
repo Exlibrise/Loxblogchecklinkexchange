67d46116aa72c5f82c0a3b6f08da383fe45b29c9
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-10-26 08:39:55 +0000

    Correct docs

--- examples/ping/gin-charge/main.go
+++ examples/ping/gin-charge/main.go
@@ -25,3 +25,10 @@ func main() {
 	}
 	// Use middleware
 	r.Use(wall.NewGinMiddleware(invoiceOptions, lnClient, storageClient))
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.Run() // Listen and serve on 0.0.0.0:8080
+}

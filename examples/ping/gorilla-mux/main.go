1b418e5ee4c510f93f2bdac7f06959c4ec5076b1
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-11-03 05:31:59 +0000

    Update ReferenceDataManager_server_test.pl

--- examples/ping/gorilla-mux/main.go
+++ examples/ping/gorilla-mux/main.go
@@ -22,4 +22,4 @@ func main() {
 	// Configure middleware
 	invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
 	lndOptions := ln.DefaultLNDoptions           // Address: "localhost:10009", CertFile: "tls.cert", MacaroonFile: "invoice.macaroon"
-	storageClient := storage.NewGoMap()      
+	storageClient := storage.NewGoMap()          // Local in-memory 

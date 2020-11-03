4ea2a55d597ed40e70111e60b8177d7670e973bc
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-11-03 15:17:22 +0000

    Added credits

--- examples/ping/gorilla-mux/main.go
+++ examples/ping/gorilla-mux/main.go
@@ -22,4 +22,5 @@ func main() {
 	// Configure middleware
 	invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
 	lndOptions := ln.DefaultLNDoptions           // Address: "localhost:10009", CertFile: "tls.cert", MacaroonFile: "invoice.macaroon"
-	storageClient := storage.NewGoMap()          // Local in-memory 
+	storageClient := storage.NewGoMap()          // Local in-memory cache
+	lnClient, e

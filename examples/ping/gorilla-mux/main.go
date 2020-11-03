22cdb8cbdd24f8acfddf5674a6b81c521040448c
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-11-03 17:31:18 +0000

    [Maven Release]-prepare for next development iteration

--- examples/ping/gorilla-mux/main.go
+++ examples/ping/gorilla-mux/main.go
@@ -23,4 +23,4 @@ func main() {
 	invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
 	lndOptions := ln.DefaultLNDoptions           // Address: "localhost:10009", CertFile: "tls.cert", MacaroonFile: "invoice.macaroon"
 	storageClient := storage.NewGoMap()          // Local in-memory cache
-	lnClient, e
+	lnClient, err := ln.NewLNDclient(lndO

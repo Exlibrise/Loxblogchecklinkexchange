78744b4b33ee1a3888f506570881162df36573f9
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-11-08 19:18:41 +0000

    Update hello.xml

--- examples/ping/handlerfunc/main.go
+++ examples/ping/handlerfunc/main.go
@@ -5,3 +5,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/philippgille/ln-paywall/ln"
+	"github.com/philippgille/ln-paywall/storage"
+	"github.com/philippgille/ln-paywall/wall"
+)
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "pong")
+}
+
+func withLogging(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Logged connection from %s", r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	// Configure middleware
+	invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
+	lndOptions := ln.DefaultLNDoptions           // Address: "localhost:10009", CertFile: "tls.cert", MacaroonFile: "invoice.macaroon"
+	storageClient := storage.NewGoMap()          // Local in-memory cache
+	lnClient, err := ln.NewLNDclient(lndOptions)
+	if err != nil {
+		panic(err)
+	}

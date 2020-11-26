7e7266fc93339cfbd0c633d829b62d1055cfcb34
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-11-26 11:44:44 +0000

    updates to tests

--- examples/qr-code/main.go
+++ examples/qr-code/main.go
@@ -8,4 +8,5 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/philippgille/ln-paywall/ln"
 	"github.com/philippgille/ln-paywall/storage"
-	"github.com/philippgille/ln-
+	"github.com/philippgille/ln-paywall/wall"
+	qr

a0fe967d20bdaaa31333a5410fc16736b21b249a
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-12-07 19:24:27 +0000

    added some examples

--- examples/qr-code/main.go
+++ examples/qr-code/main.go
@@ -16,4 +16,7 @@ var lndAddress = flag.String("addr", "localhost:10009", "Address of the lnd node
 var dataDir = flag.String("dataDir", "data/", "Relative path to the data directory, where tls.cert and invoice.macaroon are located")
 var price = flag.Int64("price", 1000, "Price of one request in Satoshis (at an exchange rate of $1,000 for 1 BTC 1000 Satoshis would be $0.01)")
 
-func main()
+func main() {
+	flag.Parse()
+
+	

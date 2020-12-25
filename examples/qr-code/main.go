8bf8f9bfbdfb68d3dda770463dc7ef0c5ec8d7bb
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2020-12-25 17:56:15 +0000

    glyphicons

--- examples/qr-code/main.go
+++ examples/qr-code/main.go
@@ -66,4 +66,4 @@ func main() {
 func qrHandler(c *gin.Context) {
 	data := c.Query("data")
 	if data == "" {
-		c.String(http.StatusBadRequest, "The query parameter \"da
+		c.String(http.StatusBadRequest, "The query parameter \"data\" is missing

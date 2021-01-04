9c600ad0bb75cd31f70dc8d0220592f2bc5dbf7b
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-01-04 15:14:27 +0000

    update kotlin

--- ln/charge.go
+++ ln/charge.go
@@ -16,3 +16,35 @@ import (
 type ChargeClient struct {
 	client   *http.Client
 	baseURL  string
+	apiToken string
+}
+
+// GenerateInvoice generates an invoice with the given price and memo.
+func (c ChargeClient) GenerateInvoice(amount int64, memo string) (Invoice, error) {
+	result := Invoice{}
+
+	data := make(url.Values)
+	// Possible values as documented in https://github.com/ElementsProject/lightning-charge/blob/master/README.md:
+	// msatoshi, currency, amount, description, expiry, metadata and webhook
+	// But with *either* msatoshi *or* currency + amount
+	mSatoshi := strconv.FormatInt(1000*amount, 10)
+	data.Add("msatoshi", mSatoshi)
+	data.Add("description", memo)
+
+	// Send request
+	req, err := http.NewRequest("POST", c.baseURL+"/invoice", strings.NewReader(data.Encode()))
+	if err != nil {
+		return result, err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth("api-token", c.apiToken) // This might seem strange, but it's how Lightning Charge expects it
+	stdOutLogger.Println("Creating invoice for a new API request")
+	res, err := c.client.Do(req)
+	if err != nil {
+		return result, err
+	}
+
+	// Read and deserialize response
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return result, err

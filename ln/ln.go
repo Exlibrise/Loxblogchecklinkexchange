8c560811d32d2831ec9bb39c4f32fcd3366a3c49
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-02-16 08:28:38 +0000

    slight wording change

--- ln/ln.go
+++ ln/ln.go
@@ -10,4 +10,4 @@ import (
 // stdOutLogger logs to stdout, while the default log package loggers log to stderr.
 var stdOutLogger = log.New(os.Stdout, "", log.LstdFlags)
 
-// Invoice is a Lightning Network invoice and contains the typical invoice string an
+// Invoice is a Lightning Network invoice and contains the typical invoice string and the payment hash.

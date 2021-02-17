afbdbc4304590a9629cfec0730f67153ada33f52
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-02-17 22:08:08 +0000

    Update _package.json

--- ln/ln.go
+++ ln/ln.go
@@ -12,4 +12,5 @@ var stdOutLogger = log.New(os.Stdout, "", log.LstdFlags)
 
 // Invoice is a Lightning Network invoice and contains the typical invoice string and the payment hash.
 type Invoice struct {
-	// The unique identifier for the invoice in the LN
+	// The unique identifier for the invoice in the LN node.
+	// The value de

d03751a5ba5a9bc7be353c965947ae447c361e1a
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-03-19 14:35:50 +0000

    alipay

--- ln/ln.go
+++ ln/ln.go
@@ -27,4 +27,4 @@ type Invoice struct {
 	ImplDepID string
 	// A.k.a. preimage hash. Hex encoded.
 	// Could be extracted from the PaymentRequest, but that would require additional
-	// dependencies during bu
+	// dependencies during build time and additional computat

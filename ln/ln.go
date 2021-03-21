48f7504cd4f55198de98a10db3a15409f3589a0d
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-03-21 16:26:54 +0000

    Linux 3.0.37

--- ln/ln.go
+++ ln/ln.go
@@ -28,4 +28,4 @@ type Invoice struct {
 	// A.k.a. preimage hash. Hex encoded.
 	// Could be extracted from the PaymentRequest, but that would require additional
 	// dependencies during build time and additional computation during runtime,
-	// while all Lightning Node implem
+	// while all Lightning Node implementation clients already r

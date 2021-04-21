a8fea9f5b97f8bde5a6af549507dc76449a06485
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-04-21 21:50:56 +0000

    merge in honeycomb-LTE-release history after reset to honeycomb-LTE

--- ln/ln_test.go
+++ ln/ln_test.go
@@ -8,4 +8,5 @@ import (
 
 // TestHashPreimage tests if the result of the HashPreimage function is correct.
 func TestHashPreimage(t *testing.T) {
-	// Correct preimage form, taken from a payment JSON in ln
+	// Correct preimage form, taken from a payment JSON in lnd
+	preimageHex := "119969c2338

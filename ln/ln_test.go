234c1679a5549308e93d09bb712a2f03627a6490
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-04-22 15:06:16 +0000

    more migration

--- ln/ln_test.go
+++ ln/ln_test.go
@@ -9,4 +9,4 @@ import (
 // TestHashPreimage tests if the result of the HashPreimage function is correct.
 func TestHashPreimage(t *testing.T) {
 	// Correct preimage form, taken from a payment JSON in lnd
-	preimageHex := "119969c2338
+	preimageHex := "119969c2338798cd56708126b5d6c0

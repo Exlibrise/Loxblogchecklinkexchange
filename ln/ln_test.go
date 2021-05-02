673ee31d9cdc82a3ccc2e4ee8c2c142572f22aa8
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-05-02 09:50:09 +0000

    ilan

--- ln/ln_test.go
+++ ln/ln_test.go
@@ -14,4 +14,4 @@ func TestHashPreimage(t *testing.T) {
 	expected := "bf3e0e73d4bb1ee9d68ca8d1078213d059e23d6e1c8a14b3df93faf87aa4fed3"
 	actual, err := ln.HashPreimage(preimageHex)
 	if err != nil {
-		t.
+		t.Errorf("An error occ

8d1ae40c7a9dcf96613c177e0a89afdca9bacde9
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-05-31 16:37:10 +0000

    ReadMe Update

--- ln/lnd.go
+++ ln/lnd.go
@@ -45,4 +45,4 @@ func (c LNDclient) GenerateInvoice(amount int64, memo string) (Invoice, error) {
 // CheckInvoice takes an invoice ID (LN node implementation specific) and checks if the corresponding invoice was settled.
 // An error is returned if no corresponding invoice was found.
 // False is returned if the invoice isn't settled.
-func (c L
+func (c LNDclient) CheckInvoice(id s

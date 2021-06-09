0fcee2930125546df658b43fdac2298d3fbe9477
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-06-09 14:47:47 +0000

    Back to development: 0.4

--- ln/lnd.go
+++ ln/lnd.go
@@ -72,4 +72,4 @@ func (c LNDclient) CheckInvoice(id string) (bool, error) {
 	return true, nil
 }
 
-// Pay pays the invoice and returns the preimage (hex encoded) on success, or
+// Pay pays the invoice and returns the preimage (hex encoded) on success, or an error on failure

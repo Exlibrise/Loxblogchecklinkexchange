bcfa5d19a9c541b6733f97f66deda7c37a5e64a3
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-06-10 18:53:35 +0000

    x86_32, entry: Store badsys error code in %eax

--- ln/lnd.go
+++ ln/lnd.go
@@ -74,4 +74,4 @@ func (c LNDclient) CheckInvoice(id string) (bool, error) {
 
 // Pay pays the invoice and returns the preimage (hex encoded) on success, or an error on failure.
 func (c LNDclient) Pay(invoice string) (string, error) {
-	// Decode pay
+	// Decode payment request (a.k.a. invo

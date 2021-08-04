2e7ffb23b0934ed56c3812f576f2c74d9ce8326b
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-08-04 20:42:27 +0000

    docs: fix typos

--- pay/docs.go
+++ pay/docs.go
@@ -1,2 +1,6 @@
 /*
-Package pay contains the client-side functionality for easy use of APIs that are paywalled with ln-paywall or other compatible paywall implemen
+Package pay contains the client-side functionality for easy use of APIs that are paywalled with ln-paywall or other compatible paywall implementations.
+
+Usage
+
+	packa

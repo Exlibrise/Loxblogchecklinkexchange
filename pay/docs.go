bbb56b3989a8eb4d220ec074acc1d863f50f6f1f
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-08-12 15:18:41 +0000

    Update translation file

--- pay/docs.go
+++ pay/docs.go
@@ -16,4 +16,6 @@ Usage
 	func main() {
 		// Set up client
 		lndOptions := ln.LNDoptions{ // Default address: "localhost:10009", CertFile: "tls.cert"
-			MacaroonFile: "admin.macaroon", // admin.macaroon is required for maki
+			MacaroonFile: "admin.macaroon", // admin.macaroon is required for making payments
+		}
+		lnClient, er

455c20b3668501affc62f1ddbd0253fd1d4beced
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-08-24 19:13:10 +0000

    f2fs crypto: fix spelling typo in comment

--- pay/pay.go
+++ pay/pay.go
@@ -9,4 +9,5 @@ import (
 // LNclient is the abstraction of a Lightning Network node client for paying LN invoices.
 type LNclient interface {
 	// Pay pays the invoice and returns the preimage on success, or an error on failure.
-	Pay(invoice 
+	Pay(invoice string) (string, error)
+}

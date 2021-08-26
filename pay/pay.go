4bd215ccb0178b8197d42b810d7f230a1d291fe7
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-08-26 10:26:02 +0000

    Update npm dependencies

--- pay/pay.go
+++ pay/pay.go
@@ -13,4 +13,4 @@ type LNclient interface {
 }
 
 // Client is an HTTP client, which handles "Payment Required" interruptions transparently.
-// It must be initially set up with 
+// It must be initially set up with a connection the Lightning Network n

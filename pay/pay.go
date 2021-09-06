83d86f46872fab2b49b2485fe6e2b2481980d8b6
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-09-06 06:37:01 +0000

    added error checking

--- pay/pay.go
+++ pay/pay.go
@@ -24,4 +24,5 @@ type Client struct {
 
 // Get sends an HTTP GET request to the given URL and automatically handles the required payment in the background.
 // It does this by sending its own request to the URL + path of the given request
-// to trigger a "402 Payment Required" re
+// to trigger a "402 Payment Required" response with an invoice.
+// I

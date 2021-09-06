7809cefebaa35f520aaaa95c473291b6285812a9
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-09-06 21:59:45 +0000

    continued tweaking of html.

--- pay/pay.go
+++ pay/pay.go
@@ -25,4 +25,5 @@ type Client struct {
 // Get sends an HTTP GET request to the given URL and automatically handles the required payment in the background.
 // It does this by sending its own request to the URL + path of the given request
 // to trigger a "402 Payment Required" response with an invoice.
-// It then pays the invoice via the configured Lightning Netwo
+// It then pays the invoice via the configured Lightning Network node.
+// Finally it se

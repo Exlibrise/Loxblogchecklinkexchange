3ef5eaf7cd74859bb1bfbbcf79f308e3987e55e9
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-09-08 06:43:33 +0000

    dt

--- pay/pay.go
+++ pay/pay.go
@@ -26,4 +26,4 @@ type Client struct {
 // It does this by sending its own request to the URL + path of the given request
 // to trigger a "402 Payment Required" response with an invoice.
 // It then pays the invoice via the configured Lightning Network node.
-// Finally it sends the originally intend
+// Finally it sends the originally intended (given) request with 

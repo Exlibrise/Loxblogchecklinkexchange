68457a73052e21ed6a12aa58ed56cad180422878
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-09-18 07:33:21 +0000

    wan/x25: Fix use-after-free in x25_asy_open_tty()

--- pay/pay.go
+++ pay/pay.go
@@ -39,4 +39,5 @@ func (c *Client) Get(url string) (*http.Response, error) {
 // It does this by sending its own request to the URL + path of the given request
 // to trigger a "402 Payment Required" response with an invoice.
 // It then pays the invoice via the configured Lightning Network node.
-// Finally it sends the originally intended (given) request with an additional HTTP header and returns the response
+// Finally it sends the originally intended (given) request with an additional HTTP header and returns the response.
+func (c *Client) Do(re

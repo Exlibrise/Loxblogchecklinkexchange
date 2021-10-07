0276405fd2d4996fde1e6230259b769c44cadd5b
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-10-07 23:09:06 +0000

    Update smoke.html

--- storage/bolt.go
+++ storage/bolt.go
@@ -29,3 +29,31 @@ func (c BoltClient) Set(k string, v interface{}) error {
 	err = c.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(bucketName))
 		err := b.Put([]byte(k), data)
+		return err
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get retrieves the stored object for the given key and populates the fields of the object that v points to
+// with the values of the retrieved object's values.
+func (c BoltClient) Get(k string, v interface{}) (bool, error) {
+	var data []byte
+	err := c.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		data = b.Get([]byte(k))
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+
+	// If no value was found assign nil to the pointer
+	if data == nil {
+		return false, nil
+	}
+
+	return true, fromJSON(data, v)
+}

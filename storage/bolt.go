56dfd2b011ad7504bcd40cbbff3c0279fac20bac
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-10-08 15:43:32 +0000

    Add bin

--- storage/bolt.go
+++ storage/bolt.go
@@ -57,3 +57,37 @@ func (c BoltClient) Get(k string, v interface{}) (bool, error) {
 
 	return true, fromJSON(data, v)
 }
+
+// BoltOptions are the options for the BoltClient.
+type BoltOptions struct {
+	// Path of the DB file.
+	// Optional ("ln-paywall.db" by default).
+	Path string
+}
+
+// DefaultBoltOptions is a BoltOptions object with default values.
+// Path: "ln-paywall.db"
+var DefaultBoltOptions = BoltOptions{
+	Path: "ln-paywall.db",
+}
+
+// NewBoltClient creates a new BoltClient.
+// Note: Bolt uses an exclusive write lock on the database file so it cannot be shared by multiple processes.
+// For preventing clients from cheating (reusing preimages across different endpoints / middlewares that use
+// different Bolt DB files) and for the previous mentioned reason you should use only one BoltClient.
+// For example:
+//  // ...
+//  storageClient, err := storage.NewBoltClient(storage.DefaultBoltOptions) // Uses file "ln-paywall.db"
+//  if err != nil {
+//      panic(err)
+//  }
+//  cheapPaywall := wall.NewGinMiddleware(cheapInvoiceOptions, lnClient, storageClient)
+//  expensivePaywall := wall.NewGinMiddleware(expensiveInvoiceOptions, lnClient, storageClient)
+//  router.GET("/ping", cheapPaywall, pingHandler)
+//  router.GET("/compute", expensivePaywall, computeHandler)
+//  // ...
+// If you want to start an additional web service, this would be an additional process, so you can't use the same
+// DB file. You should look into the other storage options in this case, for example Redis.
+//
+// Don't worry about closing the Bolt DB, the middleware opens it once and uses it for the duration of its lifetime.
+// When the web service is stopped, the DB file lock is released automatically.

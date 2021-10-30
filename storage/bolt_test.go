62a14e94005ee5342a1c67c20dce9004ef553b61
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-10-30 12:27:55 +0000

    fix overflow

--- storage/bolt_test.go
+++ storage/bolt_test.go
@@ -39,4 +39,4 @@ func TestBoltClient(t *testing.T) {
 }
 
 // TestBoltClientConcurrent launches a bunch of goroutines that concurrently work with one BoltClient.
-// The BoltClient works with a single file
+// The BoltClient works with a single file, so everything sho

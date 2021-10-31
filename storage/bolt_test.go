ad4040c4a08ed5e7812a0253433cc2472b70b5f7
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-10-31 10:04:34 +0000

    Update release info

--- storage/bolt_test.go
+++ storage/bolt_test.go
@@ -40,4 +40,4 @@ func TestBoltClient(t *testing.T) {
 
 // TestBoltClientConcurrent launches a bunch of goroutines that concurrently work with one BoltClient.
 // The BoltClient works with a single file, so everything should be locked properly.
-// The 
+// The locking is implemented in t

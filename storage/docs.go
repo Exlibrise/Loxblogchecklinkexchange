32336c359c0946b294d03e13eabde1e1b3962856
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-11-17 20:50:44 +0000

    ext4: fix data offset overflow in ext4_xattr_fiemap() on 32-bit archs

---
+++ storage/docs.go
@@ -0,0 +1,8 @@
+
+/*
+Package storage contains storage implementations and related options.
+
+These storage implementations satisfy the wall.StorageClient interface.
+You can use one of them or implement your own.
+*/
+package storage

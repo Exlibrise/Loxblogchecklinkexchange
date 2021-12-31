af642a8e60b00cef3e8e241df892ffde925a297f
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2021-12-31 16:27:04 +0000

    Fix a typo in comment

--- storage/redis_test.go
+++ storage/redis_test.go
@@ -4,3 +4,21 @@ package storage_test
 import (
 	"log"
 	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/go-redis/redis"
+
+	"github.com/philippgille/ln-paywall/ln"
+	"github.com/philippgille/ln-paywall/storage"
+	"github.com/philippgille/ln-paywall/wall"
+)
+
+// Don't use the default number ("0"),
+// which could lead to valuable data being deleted when a developer accidentally runs the test with valuable data in DB 0.
+var testDbNumber = 15 // 16 DBs by default (unchanged config), starting with 0
+
+// TestRedisClientImpl tests if the RedisClient struct implements the StorageClient interface.
+// This doesn't happen at runtime, but at compile time.
+func TestRedisClientImpl(t *testing.T) {
+	t.SkipNow()

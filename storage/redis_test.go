fbe73e55bfd35a43a24427c345a8de0b17103b9c
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-01 15:21:11 +0000

    MVC

--- storage/redis_test.go
+++ storage/redis_test.go
@@ -22,3 +22,30 @@ var testDbNumber = 15 // 16 DBs by default (unchanged config), starting with 0
 // This doesn't happen at runtime, but at compile time.
 func TestRedisClientImpl(t *testing.T) {
 	t.SkipNow()
+	invoiceOptions := wall.InvoiceOptions{}
+	lnClient := ln.LNDclient{}
+	redisClient := storage.RedisClient{}
+	wall.NewHandlerFuncMiddleware(invoiceOptions, lnClient, redisClient)
+	wall.NewHandlerMiddleware(invoiceOptions, lnClient, redisClient)
+	wall.NewGinMiddleware(invoiceOptions, lnClient, redisClient)
+}
+
+// TestRedisClient tests if reading and writing to the storage works properly.
+//
+// Note: This test is only executed if the initial connection to Redis works.
+func TestRedisClient(t *testing.T) {
+	if !checkRedisConnection(testDbNumber) {
+		t.Skip("No connection to Redis could be established. Probably not running in a proper test environment.")
+	}
+
+	deleteRedisDb(testDbNumber) // Prep for previous test runs
+	redisOptions := storage.RedisOptions{
+		DB: testDbNumber,
+	}
+	redisClient := storage.NewRedisClient(redisOptions)
+
+	testStorageClient(redisClient, t)
+}
+
+// TestRedisClientConcurrent launches a bunch of goroutines that concurrently work with the Redis client.
+func TestRedisClientConcurrent(t *testing.T) {

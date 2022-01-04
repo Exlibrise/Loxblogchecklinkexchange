d8cad3c27ece3c72774d1431494536fad55dec98
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-04 08:25:54 +0000

    improved test coverage

--- storage/storage_test.go
+++ storage/storage_test.go
@@ -9,3 +9,23 @@ import (
 
 	"github.com/philippgille/ln-paywall/wall"
 )
+
+type foo struct {
+	Bar string
+}
+
+// testStorageClient tests if reading from and writing to the storage works properly.
+func testStorageClient(storageClient wall.StorageClient, t *testing.T) {
+	key := strconv.FormatInt(rand.Int63(), 10)
+
+	// Initially the key shouldn't exist
+	found, err := storageClient.Get(key, new(foo))
+	if err != nil {
+		t.Error(err)
+	}
+	if found {
+		t.Errorf("A value was found, but no value was expected")
+	}
+
+	// Store an object
+	val := foo{

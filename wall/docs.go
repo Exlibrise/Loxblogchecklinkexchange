c3625ae63bb316b064c0d1cefc1f983f088f68b1
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-07 12:50:46 +0000

    lolol

--- wall/docs.go
+++ wall/docs.go
@@ -1,4 +1,5 @@
 /*
 Package wall contains all paywall-related code.
 
-This is the package you need to use for creating a middleware for one of the supported handlers / route
+This is the package you need to use for creating a middleware for one of the supported handlers / routers / frameworks.
+For creating a

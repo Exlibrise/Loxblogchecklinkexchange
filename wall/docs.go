53922815bd6e3236672f6010cdc1aa10af3d0040
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-07 10:29:56 +0000

    ignore .DS_Store files

--- wall/docs.go
+++ wall/docs.go
@@ -1,4 +1,4 @@
 /*
 Package wall contains all paywall-related code.
 
-This is the package you need to use for creating a middleware for one
+This is the package you need to use for creating a middleware for one of the supported handlers / route

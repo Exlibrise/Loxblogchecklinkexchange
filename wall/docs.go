59bd6edf872145bf229aca706373c5a5e54b2aa8
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-08 20:05:48 +0000

    speedup

--- wall/docs.go
+++ wall/docs.go
@@ -2,4 +2,5 @@
 Package wall contains all paywall-related code.
 
 This is the package you need to use for creating a middleware for one of the supported handlers / routers / frameworks.
-For creating a middleware you only need to call one of the provided 
+For creating a middleware you only need to call one of the provided factory functions,
+bu

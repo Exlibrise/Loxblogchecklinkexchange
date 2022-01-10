d5f11e5819163dc4fd73d8d9735f5d67675e7108
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-10 09:19:39 +0000

    wakeup: add toggles for wlan wakelocks. They are all enabled by default, it's up to the user and I provide no support if Wi-Fi stops working normally without these locks enabled. This is for advanced users

--- wall/docs.go
+++ wall/docs.go
@@ -3,4 +3,4 @@ Package wall contains all paywall-related code.
 
 This is the package you need to use for creating a middleware for one of the supported handlers / routers / frameworks.
 For creating a middleware you only need to call one of the provided factory functions,
-but all functions require a storage client (an implementation of the wall.StorageCl
+but all functions require a storage client (an implementation of the wall.StorageClient interface) as parame

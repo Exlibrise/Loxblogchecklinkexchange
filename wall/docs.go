60ab8992b7fe56fdb4dd74b02daa4617863a0100
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-10 20:13:42 +0000

    Fix assignment

--- wall/docs.go
+++ wall/docs.go
@@ -3,4 +3,5 @@ Package wall contains all paywall-related code.
 
 This is the package you need to use for creating a middleware for one of the supported handlers / routers / frameworks.
 For creating a middleware you only need to call one of the provided factory functions,
-but all functions require a storage client (an implementation of the wall.StorageClient interface) as parame
+but all functions require a storage client (an implementation of the wall.StorageClient interface) as parameter.
+You can either pick on

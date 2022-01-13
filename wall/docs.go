bb288a8dc2cd7409a607871574b6a32160266130
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-13 09:41:35 +0000

    storage

--- wall/docs.go
+++ wall/docs.go
@@ -4,4 +4,4 @@ Package wall contains all paywall-related code.
 This is the package you need to use for creating a middleware for one of the supported handlers / routers / frameworks.
 For creating a middleware you only need to call one of the provided factory functions,
 but all functions require a storage client (an implementation of the wall.StorageClient interface) as parameter.
-You can either pick one from the storage package (https://www.godoc.org/github.com/philippgil
+You can either pick one from the storage package (https://www.godoc.org/github.com/philippgille/ln-paywall/storage), or impl

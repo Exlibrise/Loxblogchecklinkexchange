b093bd30707c506479d346ddffefe812bdb9ba47
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-22 07:10:26 +0000

    added asserts

--- wall/docs.go
+++ wall/docs.go
@@ -25,4 +25,5 @@ For more examples check out the "examples" directory in the GitHub repository of
 	func main() {
 		r := gin.Default()
 
-		// Configure middlewar
+		// Configure middleware
+		invoiceOptio

55d854ccccdd2dd984d71a7e3647215e9d1cccd6
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-22 07:24:05 +0000

    adverbs

--- wall/docs.go
+++ wall/docs.go
@@ -26,4 +26,4 @@ For more examples check out the "examples" directory in the GitHub repository of
 		r := gin.Default()
 
 		// Configure middleware
-		invoiceOptio
+		invoiceOptions := wall.DefaultInvoi

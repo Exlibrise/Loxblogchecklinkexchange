2a806ca34cde1977c257dbc30fe4a8b6886ac41e
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-23 18:25:11 +0000

    Implement EJB scheduling.

--- wall/docs.go
+++ wall/docs.go
@@ -26,4 +26,5 @@ For more examples check out the "examples" directory in the GitHub repository of
 		r := gin.Default()
 
 		// Configure middleware
-		invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satosh
+		invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
+		lndOptions :=

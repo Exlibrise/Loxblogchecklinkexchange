5f867c277f1d718d70b8155d779af3de4e1ac885
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-26 07:52:43 +0000

    Remove useless dependencies

--- wall/docs.go
+++ wall/docs.go
@@ -28,4 +28,4 @@ For more examples check out the "examples" directory in the GitHub repository of
 		// Configure middleware
 		invoiceOptions := wall.DefaultInvoiceOptions // Price: 1 Satoshi; Memo: "API call"
 		lndOptions := ln.DefaultLNDoptions           // Address: "localhost:10009", CertFile: "tls.cert", MacaroonFile: "invoice.macaroon"
-		storageClient := storage
+		storageClient := storage.NewGoMap()          // Local

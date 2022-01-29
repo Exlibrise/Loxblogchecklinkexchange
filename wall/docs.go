ede92199aed3cb304866432b7dac79f74c12da7f
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-01-29 09:12:52 +0000

    Help debugging with MSys2 by optionally executing bash with strace

--- wall/docs.go
+++ wall/docs.go
@@ -37,3 +37,4 @@ For more examples check out the "examples" directory in the GitHub repository of
 		r.Use(wall.NewGinMiddleware(invoiceOptions, lnClient, storageClient))
 
 		r.GET("/ping", func(c *gin.Context) {
+			c.String(http.St

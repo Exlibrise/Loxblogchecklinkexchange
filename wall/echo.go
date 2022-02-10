6942548c957eff0f9393c22c8a1f98edb381cd94
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-02-10 14:13:25 +0000

    SCSI: ibmvscsi: Fix host config length field overflow

--- wall/echo.go
+++ wall/echo.go
@@ -15,3 +15,27 @@ func NewEchoMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, storage
 		if skipper == nil {
 			skipper = middleware.DefaultSkipper
 		}
+		return func(ctx echo.Context) error {
+			if skipper(ctx) {
+				return next(ctx)
+			}
+			fa := echoAbstraction{
+				ctx:         ctx,
+				nextHandler: next,
+			}
+			return commonHandler(fa, invoiceOptions, lnClient, storageClient)
+		}
+	}
+}
+
+type echoAbstraction struct {
+	ctx         echo.Context
+	nextHandler echo.HandlerFunc
+}
+
+func (fa echoAbstraction) getPreimageFromHeader() string {
+	return fa.ctx.Request().Header.Get("x-preimage")
+}
+
+func (fa echoAbstraction) respondWithError(err error, errorMsg string, statusCode int) {
+	fa.ctx.String(statusCode, errorMsg)

cd81978483781df38b79b2aeaf301eed4d785a43
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-04-24 11:41:09 +0000

    fixed log

--- wall/middleware.go
+++ wall/middleware.go
@@ -36,4 +36,4 @@ var DefaultInvoiceOptions = InvoiceOptions{
 // StorageClient is an abstraction for different storage client implementations.
 // A storage client must be able to store and retrieve invoiceMetaData objects.
 type StorageClient interface {
-	// Set s
+	// Set stores the given invoiceMetaD

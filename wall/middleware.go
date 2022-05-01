aa3a2e610a8d82571d084f6a9f8dc7179c2708f5
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-05-01 19:16:05 +0000

    update status

--- wall/middleware.go
+++ wall/middleware.go
@@ -39,4 +39,4 @@ type StorageClient interface {
 	// Set stores the given invoiceMetaData for the given preimage hash.
 	Set(string, interface{}) error
 	// Get retrieves the invoiceMetaData for the given preimage hash
-	// and populates the fields of
+	// and populates the fields of the object that 

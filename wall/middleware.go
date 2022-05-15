6e10f0ad9051723a262f74ebb9e3af15c8f4d4e9
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-05-15 21:55:56 +0000

    update translation files from transifex [skip ci]

--- wall/middleware.go
+++ wall/middleware.go
@@ -41,4 +41,5 @@ type StorageClient interface {
 	// Get retrieves the invoiceMetaData for the given preimage hash
 	// and populates the fields of the object that the passed pointer
 	// points to with the values of the retrieved object's values.
-	// If no object is found it returns (false, ni
+	// If no object is found it returns (false, nil).
+	Get(string, interfac

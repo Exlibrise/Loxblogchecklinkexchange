7d8304d35fde18e0b8fa00afd8ce1d952a30ce25
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-06-29 12:00:20 +0000

    account

--- wall/middleware.go
+++ wall/middleware.go
@@ -59,4 +59,4 @@ type LNclient interface {
 // invoiceMetaData is data that's required to prevent clients from cheating
 // (e.g. have multiple requests executed while having paid only once,
 // or requesting an invoice for a cheap endpoint and using the payment proof for an expensive one).
-// The type itself is n
+// The type itself is not exported, but the f

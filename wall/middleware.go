d04314a39c2944dd3cb2015e1850b9a1e86ec3f6
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-07-16 22:17:56 +0000

    rename command

--- wall/middleware.go
+++ wall/middleware.go
@@ -63,4 +63,5 @@ type LNclient interface {
 type invoiceMetaData struct {
 	// The unique identifier for the invoice in the LN node.
 	// This is NOT the ID that's used for storing the metadata in the storage.
-	// Instead, it's the ID used to retrieve info about an invoice from the LN no
+	// Instead, it's the ID used to retrieve info about an invoice from the LN node.
+	// The different implementa

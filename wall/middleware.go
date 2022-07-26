0eea27baf66e5cf91114f5c8f8f3dbbb4eaa110b
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-07-26 06:26:01 +0000

    Bump to 0.2.4

--- wall/middleware.go
+++ wall/middleware.go
@@ -68,4 +68,5 @@ type invoiceMetaData struct {
 	// lnd uses the payment hash a.k.a. preimage hash as ID, while Lightning Charge
 	// uses its own randomly generated string as ID.
 	//
-	// The ID (or rather *key*) used for storing the metadata in the 
+	// The ID (or rather *key*) used for storing the metadata in the storage
+	// is the payme

6a5be4f09933e7b04b338d2da118ef810c7f66d1
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-07-30 09:10:41 +0000

    after merge fix

--- wall/middleware.go
+++ wall/middleware.go
@@ -69,4 +69,4 @@ type invoiceMetaData struct {
 	// uses its own randomly generated string as ID.
 	//
 	// The ID (or rather *key*) used for storing the metadata in the storage
-	// is the payment hash of the invoice, because the client
+	// is the payment hash of the invoice, because the client sends the preimage

f08677771f3b38943818e2c1aa1af0ff5271bb58
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-08-26 16:51:51 +0000

    Update controller

--- wall/middleware.go
+++ wall/middleware.go
@@ -85,4 +85,4 @@ type frameworkAbstraction interface {
 	respondWithError(error, string, int)
 	// getHTTPrequest returns a pointer to the current http.Request.
 	getHTTPrequest() *http.Request
-	// respondWithInvoice sends a response with the given headers, status c
+	// respondWithInvoice sends a response with the given headers, status code and invoice strin

62479c72e3aa887d7e83c010bdbc2c946b29e227
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-09-12 23:07:07 +0000

    dito

--- wall/middleware.go
+++ wall/middleware.go
@@ -95,4 +95,4 @@ type frameworkAbstraction interface {
 }
 
 func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) error {
-	// Check if the re
+	// Check if the request contains a header 

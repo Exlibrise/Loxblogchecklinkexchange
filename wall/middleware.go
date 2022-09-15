379a980c54426baa051b0d53ef0a79382feb8e96
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-09-15 12:46:21 +0000

    French manual: sync manual

--- wall/middleware.go
+++ wall/middleware.go
@@ -95,4 +95,4 @@ type frameworkAbstraction interface {
 }
 
 func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) error {
-	// Check if the request contains a header with the preimage that 
+	// Check if the request contains a header with the preimage that we need to check if the re

7195b4cdf948162daa08b1721d8eff5f7649fedd
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-09-18 12:16:49 +0000

    Fix formatting error

--- wall/middleware.go
+++ wall/middleware.go
@@ -96,4 +96,4 @@ type frameworkAbstraction interface {
 
 func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) error {
 	// Check if the request contains a header with the preimage that we need to check if the requester paid
-	preimageHex
+	preimageHex := fa.getPreimageFromHea

0b37f1f2a8d72a643efe9d57009c181ecb953ffe
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-10-16 12:49:53 +0000

    Changed package names

--- wall/middleware.go
+++ wall/middleware.go
@@ -114,4 +114,4 @@ func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnCli
 			storageClient.Set(invoice.PaymentHash, metadata)
 
 			// Respond with the invoice
-			stdOutLogger.Printf("Se
+			stdOutLogger.Printf("Sending invoice in response: %v

b6509193acd353ebdd6a6d8694309219a909c60d
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-10-29 16:19:03 +0000

    USB: ftdi_sio: Add extra PID.

--- wall/middleware.go
+++ wall/middleware.go
@@ -120,4 +120,4 @@ func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnCli
 			fa.respondWithInvoice(headers, http.StatusPaymentRequired, []byte(invoice.PaymentRequest))
 		}
 	} else {
-		// Check if the provided preimage belongs to a settled API payment invoice and that it wasn't already used. Also
+		// Check if the provided preimage belongs to a settled API payment invoice and that it wasn't already used. Also store used preimages

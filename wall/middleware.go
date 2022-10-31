0b4c93916fe52ea64e08e4f675b2eedc8e551bc6
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-10-31 06:50:44 +0000

    update js

--- wall/middleware.go
+++ wall/middleware.go
@@ -120,4 +120,5 @@ func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnCli
 			fa.respondWithInvoice(headers, http.StatusPaymentRequired, []byte(invoice.PaymentRequest))
 		}
 	} else {
-		// Check if the provided preimage belongs to a settled API payment invoice and that it wasn't already used. Also store used preimages
+		// Check if the provided preimage belongs to a settled API payment invoice and that it wasn't already used. Also store used preimages.
+		invalidPreimageMsg, err :

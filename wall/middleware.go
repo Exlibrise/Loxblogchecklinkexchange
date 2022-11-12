fa0e23b77f7c930c443fc74828d4bcf9254c58ed
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-11-12 09:41:18 +0000

    Bump to 0.0.13

--- wall/middleware.go
+++ wall/middleware.go
@@ -146,4 +146,5 @@ func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnCli
 
 // handlePreimage does the following:
 // 1) Validate the preimage format (encoding, length)
-// 2) Check if the invoice metadata exists i
+// 2) Check if the invoice metadata exists in the storage
+// 3) Chec

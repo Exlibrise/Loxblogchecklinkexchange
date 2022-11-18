c9a9731c2637457f280e696f978157e0004a5077
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-11-18 10:34:03 +0000

    remove return

--- wall/middleware.go
+++ wall/middleware.go
@@ -154,4 +154,4 @@ func commonHandler(fa frameworkAbstraction, invoiceOptions InvoiceOptions, lnCli
 // Note: The payment hash (a.k.a. preimage hash) can be calculated from the preimage.
 //
 // Returns a string and an error.
-// The string contains detailed i
+// The string contains detailed info about the result in case the

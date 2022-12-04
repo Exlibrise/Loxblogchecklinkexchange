7dfa3ec850970b32d812e726dfcc7b9a940b743b
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2022-12-04 14:25:35 +0000

    bump build

--- wall/middleware.go
+++ wall/middleware.go
@@ -181,4 +181,4 @@ func handlePreimage(req *http.Request, storageClient StorageClient, lnClient LNc
 
 	// 2. Check if the preimage hash exists in the storage
 	if !found {
-		return "You seem to have sent an inval
+		return "You seem to have sent an invalid preimage or one that

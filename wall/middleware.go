4a786d08c88fd6dbc9fb28fdceed2b05d697dc76
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-01-19 16:25:34 +0000

    sync with transiflex

--- wall/middleware.go
+++ wall/middleware.go
@@ -238,4 +238,5 @@ func validatePreimageFormat(preimageHex string) string {
 
 func assignDefaultValues(invoiceOptions InvoiceOptions) InvoiceOptions {
 	// InvoiceOptions
-	if invoiceOptions.
+	if invoiceOptions.Price <= 0 {
+		invoiceOptions.Pr

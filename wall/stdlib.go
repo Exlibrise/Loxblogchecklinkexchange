c1256ab734c745ac7d3557401cb7a1e313d1b2f9
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-01-31 06:33:34 +0000

    Quick Fix

--- wall/stdlib.go
+++ wall/stdlib.go
@@ -5,4 +5,5 @@ import (
 )
 
 // NewHandlerFuncMiddleware returns a function which you can use within an http.HandlerFunc chain.
-func NewHandlerFuncMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) func(http.HandlerFunc) http.H
+func NewHandlerFuncMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) func(http.HandlerFunc) http.HandlerFunc {
+	return func(ne

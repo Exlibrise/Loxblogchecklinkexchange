db7aed11de822c9467af7cced51d8f49790cdd1e
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-01-31 16:20:49 +0000

    Create test.java

--- wall/stdlib.go
+++ wall/stdlib.go
@@ -6,4 +6,5 @@ import (
 
 // NewHandlerFuncMiddleware returns a function which you can use within an http.HandlerFunc chain.
 func NewHandlerFuncMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) func(http.HandlerFunc) http.HandlerFunc {
-	return func(next http.HandlerFunc) h
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		r

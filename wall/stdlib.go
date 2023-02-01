246446f84399e3b3f472d753d0f5d9ada889ab07
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-02-01 12:02:54 +0000

    rbd: send header version when notifying

--- wall/stdlib.go
+++ wall/stdlib.go
@@ -7,4 +7,8 @@ import (
 // NewHandlerFuncMiddleware returns a function which you can use within an http.HandlerFunc chain.
 func NewHandlerFuncMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) func(http.HandlerFunc) http.HandlerFunc {
 	return func(next http.HandlerFunc) http.HandlerFunc {
-		return createHandlerFunc(invoiceOptions, lnClient, storageClient, next
+		return createHandlerFunc(invoiceOptions, lnClient, storageClient, next)
+	}
+}
+
+// NewHandlerMi

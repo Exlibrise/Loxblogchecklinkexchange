0b65a1f83765c181a26c4a2d8d88f8f334a8c3f8
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-02-01 22:19:53 +0000

    Travis CI integration

--- wall/stdlib.go
+++ wall/stdlib.go
@@ -12,4 +12,4 @@ func NewHandlerFuncMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, 
 }
 
 // NewHandlerMiddleware returns a function which you can use within an http.Handler chain.
-func N
+func NewHandlerMiddleware(invoiceOptions I

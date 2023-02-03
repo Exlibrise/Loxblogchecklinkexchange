cbe06f68187f6d1e663ca271c29f72ea2c947a13
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-02-03 21:48:43 +0000

    package name

--- wall/stdlib.go
+++ wall/stdlib.go
@@ -13,4 +13,5 @@ func NewHandlerFuncMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, 
 
 // NewHandlerMiddleware returns a function which you can use within an http.Handler chain.
 func NewHandlerMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient) func(http.Handler) http.Handler {
-	return func(next http.Han
+	return func(next http.Handler) http.Handler {
+	

d484a408cbb560c6caf56416373a455cce9e9372
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-02-07 10:57:07 +0000

    Update documentation/WindRiverHelix/WindRiverAppCloudProjects.md

--- wall/stdlib.go
+++ wall/stdlib.go
@@ -18,4 +18,4 @@ func NewHandlerMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, stor
 	}
 }
 
-func createHandlerFunc(invoiceOptions InvoiceOptions, lnClient LNclient, storageCl
+func createHandlerFunc(invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient, next h

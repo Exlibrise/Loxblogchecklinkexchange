a23fe325c6d2f1f342ed67f9782813635c0d778c
Exlibrise <97614977+Exlibrise@users.noreply.github.com>
2023-02-08 18:34:37 +0000

    Bump to 0.7.0

--- wall/stdlib.go
+++ wall/stdlib.go
@@ -19,4 +19,5 @@ func NewHandlerMiddleware(invoiceOptions InvoiceOptions, lnClient LNclient, stor
 }
 
 func createHandlerFunc(invoiceOptions InvoiceOptions, lnClient LNclient, storageClient StorageClient, next http.HandlerFunc) func(w http.ResponseWriter, r *http.Request) {
-	invoiceOptions = assignDefaultValue
+	invoiceOptions = assignDefaultValues(invoiceOptions)
+	return f
